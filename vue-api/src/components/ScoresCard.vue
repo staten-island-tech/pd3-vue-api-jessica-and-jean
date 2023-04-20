5e85d81778172a515a454b4d426eb1b742036de6
jeanl7281 <jeanl.03.2007@gmail.com>
2023-04-20 10:16:02 -0400

    code

---
+++ vue-api/src/components/ScoresCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <h2>{{ scores.schoolname }}</h2>
+    <h3></h3>
+  </div>
+</template>
+
+<script setup>
+const props = {
+  scores: object,
+  ap_test_takers_: number,
+  total_exams_taken: number,
+  number_of_exams_with_scores_3_4_or_5: number
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue-api/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const scores = ref()
+async function getScores() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/itfs-ms3e.json')
+  let data = await res.json()
+  scores.value = data.results
+}
+onMounted(() => {
+  getScores()
+})
+</script>
+
+<style scoped>
+.container {
+  width: 80vw;
+  margin: 30px auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
