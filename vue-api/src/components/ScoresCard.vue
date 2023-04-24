a9999ee1f7c6fa87b49065c137cb76902e4cef4e
jeanl7281 <jeanl.03.2007@gmail.com>
2023-04-24 10:15:40 -0400

    code

--- vue-api/src/components/ScoresCard.vue
+++ vue-api/src/components/ScoresCard.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="card">
     <h2>{{ scores.schoolname }}</h2>
-    <h3></h3>
+    <h3>{{ id }}</h3>
   </div>
 </template>
 
 <script setup>
 const props = {
   scores: object,
+  id: number,
   ap_test_takers_: number,
   total_exams_taken: number,
   number_of_exams_with_scores_3_4_or_5: number
--- vue-api/src/views/HomeView.vue
+++ vue-api/src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
-  <div></div>
+  <div class="container">
+    <ScoresCard
+      v-for="(HighScore, index) in scores"
+      :key="HighScore.number_of_exams_with_scores_3_4_or_5"
+      :id="index + 1"
+      :scores="HighScore"
+    />
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-const scores = ref()
+import ScoresCard from '../components/ScoresCard.vue'
+const scores = ref('')
 async function getScores() {
   let res = await fetch('https://data.cityofnewyork.us/resource/itfs-ms3e.json')
   let data = await res.json()
