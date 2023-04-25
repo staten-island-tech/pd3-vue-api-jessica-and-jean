7868a2c0945f2005dcebff51eaf8321456e16de6
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2023-04-25 10:11:23 -0400

    wrverve

--- vue-api/src/views/HomeView.vue
+++ vue-api/src/views/HomeView.vue
@@ -4,25 +4,15 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-const scores = ref()
-async function getScores() {
+const School = ref('')
+async function getSchool() {
   let res = await fetch('https://data.cityofnewyork.us/resource/itfs-ms3e.json')
   let data = await res.json()
-  scores.value = data.results
+  School.value = data.dbn
 }
 onMounted(() => {
-  getScores()
+  getSchool()
 })
 </script>
 
-<style scoped>
-.container {
-  width: 80vw;
-  margin: 30px auto;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-}
-</style>
+<style scoped></style>
